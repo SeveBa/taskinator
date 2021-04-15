778f7520e54779f884fe6c36b37935d6f99a6726
Seve Barrutia <sevebarrutia@me.com>
2021-04-14 23:41:06 -0500

    added form submit backend

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
 var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+  event.preventDefault();
 
-buttonEl.addEventListener("click", createTaskHandler);
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+  console.log(event)
+};
+
+
+  formEl.addEventListener("submit", createTaskHandler);
